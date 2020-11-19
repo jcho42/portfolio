13cde91b6a88c8b5bb7ba81ef63e43d789bd7c41
jcho42 <jason.cho.301230@gmail.com>
2020-11-19 16:14:07 -0600

    feat: adds header links with smooth scroll

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -1,5 +1,5 @@
-import { Link } from 'gatsby';
 import React, { useState } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function Header() {
   const [isExpanded, toggleExpansion] = useState(false);
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className="bg-teal-700">
       <div className="flex flex-wrap items-center justify-between max-w-4xl p-4 mx-auto md:p-8">
-        <Link to="/">
+        <AnchorLink href="#home2">
           <h1 className="flex items-center text-white no-underline">
             <svg
               className="w-8 h-8 mr-2 fill-current"
@@ -18,11 +18,8 @@ function Header() {
             >
               <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
             </svg>
-            {/* <span className="text-xl font-bold tracking-tight">
-              {site.siteMetadata.title}
-            </span> */}
           </h1>
-        </Link>
+        </AnchorLink>
 
         <button
           className="items-center block px-3 py-2 text-white border border-white rounded md:hidden"
@@ -45,21 +42,37 @@ function Header() {
         >
           {[
             {
-              route: `/about`,
+              route: `#home`,
+              title: `Home`,
+            },
+            {
+              route: `#about`,
               title: `About`,
             },
             {
-              route: `/contact`,
+              route: `#projects`,
+              title: `Projects`,
+            },
+            {
+              route: `#experience`,
+              title: `Experience`,
+            },
+            {
+              route: `#blog`,
+              title: `Blog`,
+            },
+            {
+              route: `#contact`,
               title: `Contact`,
             },
           ].map((link) => (
-            <Link
+            <AnchorLink
               className="block mt-4 text-white no-underline md:inline-block md:mt-0 md:ml-6"
               key={link.title}
-              to={link.route}
+              href={link.route}
             >
               {link.title}
-            </Link>
+            </AnchorLink>
           ))}
         </nav>
       </div>
