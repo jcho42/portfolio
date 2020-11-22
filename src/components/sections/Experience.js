92d2b0e0cf9dc98f1fd65d5ac16fac0bd60cd604
jcho42 <jason.cho.301230@gmail.com>
2020-11-21 23:05:57 -0600

    feat: adds Exp basic structure

--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -1,35 +1,52 @@
 import React from 'react';
 
 import catAndHumanIllustration from '../../images/cat-and-human-illustration.svg';
-import SectionTitle from '../layouts/sectionTitle'
+import SectionTitle from '../layouts/sectionTitle';
 
 export default function Experience() {
   return (
     <section className="section-offset" id="experience">
       <div>
         <SectionTitle>Experience</SectionTitle>
-        <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
-        />
+        <div className="timeline-line">
+          <div className="w-120 left-1/3 absolute border-2 m-16">
+            <div className="timeline-icon bg-teal-500 flex items-center">
+              <img
+                alt="Cat and human sitting on a couch"
+                className="block w-1/4 mx-8 my-4"
+                src={catAndHumanIllustration}
+              />
+              <h5>Title</h5>
+            </div>
+            <div>
+              <h6>Company</h6>
+              <p>Time</p>
+            </div>
+          </div>
 
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
+          <img
+            alt="Cat and human sitting on a couch"
+            className="block w-1/2 mx-auto mb-8"
+            src={catAndHumanIllustration}
+          />
 
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
-        </p>
+          <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
+            Hey there! Welcome to your first Gatsby site.
+          </h2>
+
+          <p className="leading-loose">
+            This is a barebones starter for Gatsby styled using{` `}
+            <a
+              className="font-bold text-gray-900 no-underline"
+              href="https://tailwindcss.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tailwind CSS
+            </a>
+            , a utility-first CSS framework.
+          </p>
+        </div>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 // See https://tailwindcss.com/docs/configuration for details
 const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+var flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
 
 module.exports = {
   purge: ['./src/**/*.js'],
@@ -19,11 +21,48 @@ module.exports = {
        '1/3': '1 0 30%'
       },
       spacing: {
-        '120': '30rem'
+        '120': '30rem',
+        '19': '4.6rem'
+      },
+      borderWidth: {
+        '20': '20px'
       }
     },
   },
-  variants: {},
+  variants: {
+
+  },
+
   // https://github.com/tailwindcss/custom-forms
-  plugins: [require('@tailwindcss/custom-forms')],
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+    require('tailwindcss-pseudo-elements'),
+    // This plugin is useful in combination with tailwindcss-aspect-ratio.
+    require('tailwindcss-aspect-ratio'),
+    plugin(function ({ addUtilities }) {
+      addUtilities(
+        {
+          '.empty-content': {
+            content: "''",
+          },
+        },
+        ['before', 'after']
+      )
+    }),
+    ({ addUtilities, e, theme, variants }) => {
+      const colors = flattenColorPalette(theme('borderColor'));
+      delete colors['default'];
+
+      const colorMap = Object.keys(colors)
+        .map(color => ({
+          [`.border-t-${color}`]: {borderTopColor: colors[color]},
+          [`.border-r-${color}`]: {borderRightColor: colors[color]},
+          [`.border-b-${color}`]: {borderBottomColor: colors[color]},
+          [`.border-l-${color}`]: {borderLeftColor: colors[color]},
+        }));
+      const utilities = Object.assign({}, ...colorMap);
+
+      addUtilities(utilities, variants('borderColor'));
+    },
+  ],
 };
