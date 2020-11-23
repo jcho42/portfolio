9500caa96ce9ce35be752d10c845710199e2ec11
jcho42 <jason.cho.301230@gmail.com>
2020-11-22 18:57:00 -0600

    feat: adds footer basic structure

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -1,31 +1,39 @@
 import React from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { IconContext } from 'react-icons';
+import { FaBeer } from 'react-icons/fa';
+import { CgChevronDoubleUp } from 'react-icons/cg';
+
+import SocialIcon from './socialIcon';
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-300">
-      <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
-        <p className="text-white">
-          Created by{` `}
-          <a
-            className="font-bold no-underline"
-            href="https://bryant.io"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Jason Cho
-          </a>
-        </p>
-
-        <p>
-          <a
-            className="font-bold text-white no-underline"
-            href="https://github.com/taylorbryant/gatsby-starter-tailwind"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        </p>
+    <footer className="py-10 bg-gray-500 relative">
+      <AnchorLink href="#home">
+        <div className="flex justify-center">
+          <div className="bg-blueGray-700 p-2 cursor-pointer -top-6 absolute">
+            <IconContext.Provider value={{ color: 'white', size: '3em' }}>
+              <CgChevronDoubleUp />
+            </IconContext.Provider>
+          </div>
+        </div>
+      </AnchorLink>
+      <nav className="max-w-md p-4 mx-auto text-sm md:p-6">
+        <div className="flex justify-center">
+          <SocialIcon link="https://github.com/jcho42/portfolio">
+            <FaBeer />
+          </SocialIcon>
+          <SocialIcon link="https://github.com/jcho42/portfolio">
+            <FaBeer />
+          </SocialIcon>
+          <SocialIcon link="https://github.com/jcho42/portfolio">
+            <FaBeer />
+          </SocialIcon>
+          <SocialIcon link="https://github.com/jcho42/portfolio">
+            <FaBeer />
+          </SocialIcon>
+        </div>
+        <p className="text-white text-center mt-8">Jason Cho &#169;2020</p>
       </nav>
     </footer>
   );
---
+++ src/components/layouts/socialIcon.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IconContext } from 'react-icons';
+
+export default function SocialIcon({link, children}) {
+  return (
+    <a href={link} target="_blank"
+    rel="noopener noreferrer">
+      <div className="bg-gray-400 p-3 mx-3">
+        <IconContext.Provider value={{ color: 'white', size: '2em' }}>
+          {children}
+        </IconContext.Provider>
+      </div>
+    </a>
+  );
+}
+
+SocialIcon.propTypes = {
+  link: PropTypes.string,
+  children: PropTypes.node,
+}
