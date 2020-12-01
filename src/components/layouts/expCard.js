8a91493a424ba27cc9a555a65e7cfefeb48916b1
jcho42 <jason.cho.301230@gmail.com>
2020-11-30 20:40:58 -0600

    feat: adds exp content

--- src/components/layouts/expCard.js
+++ src/components/layouts/expCard.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import catAndHumanIllustration from '../../images/cat-and-human-illustration.svg';
 
 export default function ExpCard({logo, title, company, time}) {
   return (
-    <div className="md:w-2/3 lg:w-1/2 left-1/10 md:left-1/5 lg:left-1/3 relative border-2 mx-16 my-10">
-      <div className="timeline-icon bg-teal-500 flex items-center">
+    <div className="md:w-2/3 lg:w-1/2 left-5 sm:left-1/10 md:left-1/5 lg:left-1/3 relative border-t-4 border-t-teal-600 mx-12 mr-5 sm:mx-16 my-10">
+      <div className="timeline-icon bg-teal-600 flex items-center md:pl-20 border-l-4 border-r-4 border-l-teal-600 border-r-teal-600">
         <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/4 mx-8 my-4"
-          src={catAndHumanIllustration}
+          alt="Exp card logo"
+          className="block w-16 mx-4 my-4"
+          src={logo}
         />
-        <h5>{logo}</h5>
-        <h5>{title}</h5>
+        <h5 className="text-white">{title}</h5>
       </div>
-      <div>
+      <div className="border-l-2 border-r-2 border-b-2 border-l-orange-600 border-r-orange-600 border-b-orange-600">
         <h6>{company}</h6>
         <p>{time}</p>
       </div>
--- src/components/sections/Experience.js
+++ src/components/sections/Experience.js
@@ -1,31 +1,34 @@
 import React from 'react';
 
 import { SectionTitle, ExpCard } from '../layouts';
+import FSALogo from '../../images/FSA logo.jpg'
+import QuestLogo from '../../images/QD-logo.jpeg'
+import UICLogo from '../../images/UIC-logo.png'
 
 export default function Experience() {
   return (
     <section className="section-offset" id="experience">
       <div className="mx-auto max-w-6xl py-8 md:py-16">
         <SectionTitle>Experience</SectionTitle>
-        <div className="timeline-line py-1 mx-10">
+        <div className="timeline-line py-1 sm:mx-10">
           {[
             {
-              logo: 'logo',
-              title: 'Title',
-              company: 'Company',
-              time: 'Time'
+              logo: FSALogo,
+              title: 'Software Engineer in training',
+              company: 'Fullstack Academy',
+              time: 'Aug 2020 - Oct 2020'
             },
             {
-              logo: 'logo',
-              title: 'Title',
-              company: 'Company',
-              time: 'Time'
+              logo: QuestLogo,
+              title: 'Medical Technologist II',
+              company: 'Quest Diagnostics',
+              time: 'Aug 2015 - Jul 2020'
             },
             {
-              logo: 'logo',
-              title: 'Title',
-              company: 'Company',
-              time: 'Time'
+              logo: UICLogo,
+              title: 'Student',
+              company: 'University of Illinois at Chicago',
+              time: 'Aug 2010 - May 2014'
             },
           ].map(card => <ExpCard key={card.company} logo={card.logo} title={card.title} company={card.company} time={card.time} />)}
         </div>
