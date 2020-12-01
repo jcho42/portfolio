b04b09fd0fa74bb46f9fef2164f6ef3d69b683de
jcho42 <jason.cho.301230@gmail.com>
2020-12-01 16:48:59 -0600

    feat: add social icons and links to footer

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import { IconContext } from 'react-icons';
-import { FaBeer } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaMediumM } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi'
 import { CgChevronDoubleUp } from 'react-icons/cg';
 
 import SocialIcon from './socialIcon';
 
 const Footer = () => {
   return (
-    <footer className="py-10 bg-gray-500 relative">
+    <footer className="py-10 bg-gray-800 relative">
       <Link to="home" spy={true} smooth={true} duration={500}>
         <div className="flex justify-center">
           <div className="bg-orange-600 p-2 cursor-pointer -top-9 absolute">
@@ -21,19 +22,19 @@ const Footer = () => {
       <nav className="max-w-md p-4 mx-auto text-sm md:p-6">
         <div className="flex justify-center">
           <SocialIcon link="https://github.com/jcho42/portfolio">
-            <FaBeer />
+            <FaGithub />
           </SocialIcon>
-          <SocialIcon link="https://github.com/jcho42/portfolio">
-            <FaBeer />
+          <SocialIcon link="https://www.linkedin.com/in/jcho42/">
+            <FaLinkedin />
           </SocialIcon>
-          <SocialIcon link="https://github.com/jcho42/portfolio">
-            <FaBeer />
+          <SocialIcon link="https://jcho42.medium.com/">
+            <FaMediumM />
           </SocialIcon>
-          <SocialIcon link="https://github.com/jcho42/portfolio">
-            <FaBeer />
+          <SocialIcon link="mailto:jason.cho.301230@gmail.com?subject=Hello!">
+            <HiOutlineMail />
           </SocialIcon>
         </div>
-        <p className="text-white opacity-75 text-center mt-8">
+        <p className="text-white opacity-60 text-center mt-8">
           Jason Cho <span className="text-orange-600">&#169;2020</span>
         </p>
       </nav>
